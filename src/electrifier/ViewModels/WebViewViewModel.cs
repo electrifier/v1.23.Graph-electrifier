976a2371cb4a8458db16e8e16b2b6062b39aa023
Thorsten Jung <tajbender@users.noreply.github.com>
2022-11-12 16:36:16 +0100

    Work an WebView: design, functions

--- src/electrifier/ViewModels/WebViewViewModel.cs
+++ src/electrifier/ViewModels/WebViewViewModel.cs
@@ -9,15 +9,24 @@ using electrifier.Contracts.ViewModels;
 using Microsoft.Web.WebView2.Core;
 
 namespace electrifier.ViewModels;
-
 // TODO: Review best practices and distribution guidelines for WebView2.
-// https://docs.microsoft.com/microsoft-edge/webview2/get-started/winui
-// https://docs.microsoft.com/microsoft-edge/webview2/concepts/developer-guide
-// https://docs.microsoft.com/microsoft-edge/webview2/concepts/distribution
+
+/// <summary>
+/// 
+/// 
+/// 
+/// To <see href="https://docs.microsoft.com/microsoft-edge/webview2/get-started/winui">get started using WebView2</see>
+/// , see <see href="https://docs.microsoft.com/microsoft-edge/webview2/concepts/developer-guide">Developer guide</see>
+/// and <see href="https://docs.microsoft.com/microsoft-edge/webview2/concepts/distribution">Distribution</see>.
+/// </summary>
 public class WebViewViewModel : ObservableRecipient, INavigationAware
 {
-    // TODO: Set the default URL to display.
-    private Uri _source = new("https://docs.microsoft.com/windows/apps/");
+    /// <summary>Default URI to display</summary>
+    private const string _defaultSourceOnLaunch = @"https://docs.microsoft.com/windows/apps/";
+
+    /// <summary>Backing fields</summary>
+    private string _currentSourceForDisplay = _defaultSourceOnLaunch;
+    private Uri _source = new(_defaultSourceOnLaunch);
     private bool _isLoading = true;
     private bool _hasFailures;
 
@@ -26,10 +35,16 @@ public class WebViewViewModel : ObservableRecipient, INavigationAware
         get;
     }
 
+    public string CurrentSourceForDisplay
+    {
+        get => _currentSourceForDisplay;
+        private set => SetProperty(ref _currentSourceForDisplay, value);
+    }
+
     public Uri Source
     {
         get => _source;
-        set => SetProperty(ref _source, value);
+        private set => SetProperty(ref _source, value);
     }
 
     public bool IsLoading
@@ -59,11 +74,6 @@ public class WebViewViewModel : ObservableRecipient, INavigationAware
         get;
     }
 
-    public ICommand RetryCommand
-    {
-        get;
-    }
-
     public ICommand OpenInBrowserCommand
     {
         get;
@@ -75,37 +85,117 @@ public class WebViewViewModel : ObservableRecipient, INavigationAware
 
         BrowserBackCommand = new RelayCommand(() => WebViewService.GoBack(), () => WebViewService.CanGoBack);
         BrowserForwardCommand = new RelayCommand(() => WebViewService.GoForward(), () => WebViewService.CanGoForward);
-        ReloadCommand = new RelayCommand(() => WebViewService.Reload());
-        RetryCommand = new RelayCommand(OnRetry);
+        ReloadCommand = new RelayCommand(OnReloadCommand);
         OpenInBrowserCommand = new RelayCommand(async () => await Windows.System.Launcher.LaunchUriAsync(WebViewService.Source), () => WebViewService.Source != null);
     }
 
     public void OnNavigatedTo(object parameter)
     {
-        WebViewService.NavigationCompleted += OnNavigationCompleted;
+        WebViewService.NavigationStarting += OnWebViewNavigationStarting;
+        WebViewService.NavigationCompleted += OnWebViewNavigationCompleted;
     }
 
     public void OnNavigatedFrom()
     {
         WebViewService.UnregisterEvents();
-        WebViewService.NavigationCompleted -= OnNavigationCompleted;
+        WebViewService.NavigationStarting -= OnWebViewNavigationStarting;
+        WebViewService.NavigationCompleted -= OnWebViewNavigationCompleted;
     }
 
-    private void OnNavigationCompleted(object? sender, CoreWebView2WebErrorStatus webErrorStatus)
+    private void OnWebViewNavigationStarting(object? sender, CoreWebView2NavigationStartingEventArgs naviStarting)
     {
-        IsLoading = false;
-        OnPropertyChanged(nameof(BrowserBackCommand));
-        OnPropertyChanged(nameof(BrowserForwardCommand));
-        if (webErrorStatus != default)
+        IsLoading = true;
+        CurrentSourceForDisplay = naviStarting.Uri;
+
+        if (naviStarting.IsUserInitiated)
         {
-            HasFailures = true;
+        }
+
+        if (naviStarting.IsRedirected)
+        {
         }
     }
 
-    private void OnRetry()
+    private void OnWebViewNavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs naviCompleted)
+    {
+        IsLoading = false;
+
+        OnPropertyChanged(nameof(BrowserBackCommand));
+        OnPropertyChanged(nameof(BrowserForwardCommand));
+        OnPropertyChanged(nameof(ReloadCommand));
+
+        if (naviCompleted.IsSuccess)
+        {
+            HasFailures = false;
+        }
+        else
+        {
+            var webErrorStatus = naviCompleted.WebErrorStatus;
+
+
+
+            //if (naviStarting.WebErrorStatus == CoreWebView2WebErrorStatus.HostNameNotResolved) { }
+
+
+            //var errorHtmlSource = $"<h1>Die gewünschte Seite konnte nicht geladen werden.</h1><br/><h2>Fehlerursache: <i>{naviCompleted.WebErrorStatus}</i></h2></html>";
+            //WebViewService.NavigateToString(errorHtmlSource);
+
+            HasFailures = true;
+
+            // TODO: neue URL setzen?!? um endlos-Schleife zu verhindern
+        }
+    }
+
+
+
+    private void OnReloadCommand()
     {
         HasFailures = false;
         IsLoading = true;
-        WebViewService?.Reload();
+
+        WebViewService.Reload();
+    }
+
+    private void OnTestCase()
+    {
+//        WebViewService.
+    }
+
+    // TODO: TestCases
+    // TODO: Fehler: "www.klackedieklack", "https://www.klickklackedie/"?
+    internal void BrowseTo(string stringEncodedUri)
+    {
+        HasFailures = false;
+        IsLoading = true;
+
+        if (stringEncodedUri.Where(x => (x == '.')).Count() < 2)
+        {
+            stringEncodedUri = $"www.{stringEncodedUri}";
+        }
+
+        if (stringEncodedUri.StartsWith("www."))
+        {
+            stringEncodedUri = $"https://{stringEncodedUri}";
+        }
+
+
+        if (Uri.TryCreate(stringEncodedUri, UriKind.RelativeOrAbsolute, out var newCreatedUri))
+        {
+            if (Uri.IsWellFormedUriString(stringEncodedUri, UriKind.Absolute))
+            {
+
+
+                CurrentSourceForDisplay = newCreatedUri.ToString();
+
+                // TODO: WebViewService.Navigate(newCreatedUri)
+                // WebViewService, corewebview -> Navigate()
+
+
+                Source = newCreatedUri;
+            }
+        }
+
+        IsLoading = false;
+        // TODO: Show some meaningful error
     }
 }
